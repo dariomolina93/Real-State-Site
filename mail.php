7bb8dbf8780f8871fce1294cc508403523f69d60
Dario Molina <dariomolina93>
2020-01-11 04:38:20 -0800

    fixing email bugs

--- mail.php
+++ mail.php
@@ -39,9 +39,9 @@ require 'vendor/autoload.php';
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-            $mail->SMTPAuth   = false;                                   // Enable SMTP authentication
-            //$mail->Username   = 'tinagarciarealtor@gmail.com';                     // SMTP username
-            //$mail->Password   = 'tinagarciarealtortinagarciarealtor';                               // SMTP password
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'tinagarciarealtorsite@gmail.com';                     // SMTP username
+            $mail->Password   = 'tinagarciarealtortinagarciarealtor';                               // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
             $mail->Port       = 587;                                    // TCP port to connect to
 
